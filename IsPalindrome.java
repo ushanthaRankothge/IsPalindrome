c2e8b7604b042fd6e5a44f57915972f35f9a548e
Rankaw <53351377+ushanthaRankothge@users.noreply.github.com>
2021-05-11 17:41:52 +0530

    Add files via upload

---
+++ IsPalindrome.java
@@ -0,0 +1,37 @@
+package palidrome;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ranka
+ */
+import java.util.*;
+
+public class IsPalindrome {
+    
+    String wordReverse;
+
+    //check if the string is a palidrome
+    protected boolean checkIfPalidrome(String wordEntered)
+    {
+    StringBuilder builder = new StringBuilder(wordEntered.length());
+   // builder.reverse(wordEntered.length());
+    for(int i = wordEntered.length()-1; i >= 0; i--)
+    {
+    //reverse the letters
+    builder.append(wordEntered.charAt(i));
+    wordReverse = builder.toString();
+
+    }
+    return wordEntered.equalsIgnoreCase(wordReverse);
+    
+    }
+    
+    }
+    
+
---
+++ Main.java
@@ -0,0 +1,37 @@
+package palidrome;
+
+
+import palidrome.IsPalindrome;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ranka
+ */
+public class Main {
+    public static void main (String[] args)
+    {
+    String word;
+    IsPalindrome palindrome = new IsPalindrome ();
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("please enter an word to check if it is palindrome");
+    word = scanner.next();
+    palindrome.checkIfPalidrome(word);
+    
+    if (palindrome.checkIfPalidrome(word)== true)
+    {
+        System.out.println("it is a palindrome");
+    }
+    else 
+    {
+      System.out.println("it is not a palindrome");  
+    }
+    }
+    
+}
